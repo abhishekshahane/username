fe78737fddc4bc862875488069a45a47f5e4e18e
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-02 00:35:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import func
+"""
+Code below this line is to be modified
+with the appropriate function calls.
+Below is merely an example.
+This is still work in progress
+as of now, and is not fully functional.
+"""
 func.putin("a", "main.com")
