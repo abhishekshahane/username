f18e3db9e23c538fe79e0df0fcf67bf186de418b
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-02 13:18:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,24 @@ import func
 """
 Code below this line is to be modified
 with the appropriate function calls.
-Below is merely an example.
-This is still work in progress
-as of now, and is not fully functional.
+Below is merely an example of how this can
+be used.
 """
 func.putin("a", "main.com")
 func.putin("b", "a.com")
-func.displayout("a.com")
-
+def main():
+    a = input("Would you like to display something(Y/N):  ")
+    if a == "Y":
+        web = input("Enter the exact website you want the username of:  ")
+        func.displayout(web)
+        again = input("Would you like to get another username(Y/N):  ")
+        if again == "Y":
+            main()
+        elif again == "N":
+            print("Save your work.")
+            exit()
+    elif a == "N":
+        print("Save your work.")
+        exit()
+if __name__ == "__main__":
+    main()
